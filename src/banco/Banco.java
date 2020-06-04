c2e58027e267110aea0036da4d5511df980f2fc9
William <William@Kevin-PC>
2020-06-04 06:49:05 -0500

    ATM

---
+++ src/banco/Banco.java
@@ -0,0 +1,16 @@
+package banco;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+public class Banco {
+
+    public static void main(String[] args) {
+       
+       Inicio lp = new Inicio();
+       lp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+    }
+}
+    
